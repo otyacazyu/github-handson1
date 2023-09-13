3d8f203b57e9b6993c0111ecc01e8c508f81ada5
otyacazyu <vesperia.not.xperia@gmail.com>
2023-09-13 20:41:06 +0900

    gameListにドラゴンクエストを追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> gamesList = List.of("ボンバーマン","星のカービィ","スマッシュブラザーズ","ポケモン","ゼルダの伝説","ファイナルファンタジー","ロックマン");
+        List<String> gamesList = List.of("ボンバーマン","星のカービィ","スマッシュブラザーズ","ポケモン","ゼルダの伝説","ファイナルファンタジー","ロックマン","ドラゴンクエスト");
 
         gamesList.forEach(System.out::println);
     }
